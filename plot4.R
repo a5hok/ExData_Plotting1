995e4c33a8c4732814e7934f86d6b401c8922e43
Ashok Subramanian <ashok.subramanian@gmail.com>
2015-01-12 00:02:10 +0000

    added comments for the multi plot

--- plot4.R
+++ plot4.R
@@ -18,12 +18,19 @@ png("plot4.png", height = 480, width = 480)
 # multiplot
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data, {
+	# Global Active Power plot
     plot(Global_active_power~Datetime, type="l", ylab="Global Active Power", xlab="")
+    
+    # Voltage plot
     plot(Voltage~Datetime, type="l", ylab="Voltage", xlab="datetime")
+    
+    # Energy sub metering
     plot(Sub_metering_1~Datetime, type="l", ylab="Energy sub metering", xlab="")
     lines(Sub_metering_2~Datetime, col='Red')
     lines(Sub_metering_3~Datetime, col='Blue')
     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=0.5, bty = "n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+    # Global Reactive power
     plot(Global_reactive_power~Datetime, type="l", ylab="Global_reactive_power", xlab="datetime")
 })
 
