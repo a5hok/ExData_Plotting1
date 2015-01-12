9dc834c130b68c87e5091d472f583fecb75f437e
Ashok Subramanian <ashok.subramanian@gmail.com>
2015-01-12 09:34:45 +0000

    Update plot1.R
    Made the background transparent

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ sql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
 data <- read.csv2.sql("household_power_consumption.txt", sql)
 
 ## Open plot1.png
-png("plot1.png", height = 480, width = 480)
+png("plot1.png",  bg = "transparent", height = 480, width = 480)
 
 ## Plot 1
 hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
